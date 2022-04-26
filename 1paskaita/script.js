d2f655db0cc25753a01f8c7ae408b157c7c022f8
SuphaSupha <vaiciunasedvinas@gmail.com>
2022-04-26 20:11:22 +0300

    Add variables

---
+++ 1paskaita/script.js
@@ -0,0 +1,12 @@
+// Vienos linijos komentaras
+
+/* 
+    MULTILAININIS komentras 
+*/
+
+// sukuriam kintamuosius
+let myName;  
+let age;
+// Priskiriam kintamiejam reikšmes
+myName = "Edvinas";
+age = 29;
