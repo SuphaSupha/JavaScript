db3bf4f36933b2283ed9f29dc3c38288da09751e
SuphaSupha <vaiciunasedvinas@gmail.com>
2022-04-27 21:56:45 +0300

    add upadated

--- 1paskaita/script.js
+++ 1paskaita/script.js
@@ -1,12 +1,109 @@
-// Vienos linijos komentaras
+// // Vienos linijos komentaras
 
-/* 
-    MULTILAININIS komentras 
-*/
+// /* 
+//     MULTILAININIS komentras 
+// */
 
-// sukuriam kintamuosius
-let myName;  
-let age;
-// Priskiriam kintamiejam reikšmes
-myName = "Edvinas";
-age = 29;
+// // sukuriam kintamuosius su let
+// let myName;  
+// // let age;
+// let mySurname;
+// let fullName;
+// // let result;
+// let average;
+// let percent;
+
+
+// // console.log(myName) // kai kintamsis yra sukurtas, bet neturi priskirtos reiksmes 
+
+// // Priskiriam kintamiejam reikšmes
+// myName = "Edvinas"; 
+// mySurname = "SuphaSupha";
+// // age = 29;
+// fullName = myName + " " + mySurname;
+// result = age + age + age - 9;
+// // result = result + result;
+// average = (8 + 9 + 10 + 10 + 10) / 5; 
+// // is viso 60, as atnesiai 20 = 33.33%
+// percent = (100 / 60) * 20; //33.33%
+// percentOutput = percent + "4";
+
+// // console.log(myName);
+
+// // console.log(9 + 9)
+// // console.log("9" + "9")
+
+// // console.log(percentOutput);
+// // console.log(percent + "%");
+// // console.log("$" + average);
+// // console.log(average)
+// // // console.log(age)
+// // // console.log(result)
+
+// // sukuriam kintamuosiu su const
+// // svarbu
+// const myDogName = "Mooncake";
+// const myDogAge = 1;
+// const doubleDogName = myDogName + myDogName
+// const greetings = "Sveikas! " + myDogName + "!";
+
+// // console.log(myDogName);
+// // console.log(myDogAge);
+// // console.log(doubleDogName);
+// // console.log(greetings);
+
+// //1. kada naudoti let kada const 
+// //1. naudoti let tik tada kai reiksme yra kintanti kitais atvejais naudoty const
+
+// //2. Sukurus kintamaji visada priskiraime pradinia reiksme
+// //3. string, number, boolean
+// //4. visada naudojam triguba ligybe(===) tikrina reiksme ir tipa
+// //5. visos neigiamsos reiksmes undifined, null, 0, "", false. 
+// const yes = true; // teigema reiksme
+// const no = false; 
+
+// // dviguba ligybe tikrina ar reiksme yra lygi 
+// // console.log("Edvinas"== "Edvinas")
+// // console.log(10==10)//lygu 10
+// // console.log(10 != 10)// ne, 10 yra lygu 10
+// // console.log(5 != 4) // taip 5 yra nelygu 4 
+// // console.log("5" == 5)
+// // // triguba ligybe tikrina ar reiksme yra lygi ir reiksme
+// // console.log("5" === 5)
+// // naudoti triguba ligybe ===
+
+
+
+// // console.log("4" != 4)
+// // console.log("4" !== 4)
+// // console.log("4" !=== 4)
+
+// // const number = 10; 
+// // const titleName = "Labas Vakaras";
+// // let iscolorBlack = false
+
+// // console.log(number);
+// // console.log(titleName);
+// // console.log(iscolorBlack);
+
+// const userName = "Jonas";
+// const farewell = "Viso gero "
+// const keepingInTouch = " lauksime sugrižtant"
+// const fullText = farewell + userName + keepingInTouch;
+
+// console.log(userName);
+// console.log(fullText);
+// console.log("Goodbye")
+
+//2pratimas 
+
+const number = 10 == 10;
+const age = 10 != 12;
+const years = 1992 === 1992;
+const height = 1.93 !== 1.95; 
+
+console.log(number);
+console.log(age);
+console.log(years);
+console.log(height);
+console.log("iki!");
